049511c4c3b6a76d63dcb1c3d0941785ae120cc0
rakot <rakot@USER>
2025-03-26 14:35:08 +0100

    Test unitaire pour un montant négatif pour la classe transaction

--- TestUnitaireTransaction.py
+++ TestUnitaireTransaction.py
@@ -9,3 +9,6 @@ class TestUnitaireTransaction(unittest.TestCase):
         with self.assertRaises(ValueError):
             Transaction(20, datetime.now(), "transfert", "IDC11", "IDC11")
     
+     def test_transferer_montant_negatif(self):
+        with self.assertRaises(ValueError):
+            Transaction(-100, datetime.now(), "transfert", "C002", "C001")
